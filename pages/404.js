d1bf6bc021f7ceb1917b7945ea98faf55362cc74
Sheshko Pavel <derzurb@gmail.com>
2022-12-06 13:05:17 +0300

    feat: 404 pafe link to main

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Error() {
     return (
-        <h1>
-            404 PAGE
-        </h1>
+        <div style={{ textAlign: "center" }}>
+            <h1 style={{color: "red"}}>
+                404 PAGE
+            </h1>
+            <Link href={"/"} style={{textDecoration: "none"}}>
+                Got to main page
+            </Link>
+        </div>
     )
 }
